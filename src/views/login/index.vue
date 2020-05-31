4f832b536c539ac7823c568f9abf7071af59fae3
wuchunfu <319355703@qq.com>
2020-05-31 12:24:33 +0800

    Fix default jump after login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,7 +9,7 @@
       label-position="left"
     >
       <div class="title-container">
-        <h3 class="title">Login</h3>
+        <h3 class="title">登陆</h3>
       </div>
       <el-form-item prop="username">
         <span class="svg-container">
@@ -51,11 +51,11 @@
         style="width:100%;margin-bottom:30px;"
         @click.native.prevent="handleLogin"
       >
-        Login
+        登陆
       </el-button>
       <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
+        <span style="margin-right:20px;">用户名: admin</span>
+        <span> 密码: 大于6为数的任何数字或字符</span>
       </div>
     </el-form>
   </div>
